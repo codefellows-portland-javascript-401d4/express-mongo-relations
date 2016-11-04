12880e506ca8edba41a7099e0b20b93103a3a661
nsmeds <niilosmeds@gmail.com>
2016-11-04 16:50:33 -0700

    Built routes and models; working on relations

---
+++ server.js
@@ -0,0 +1,9 @@
+require('./lib/setup-mongoose');
+const app = require('./lib/app');
+const port = process.env.PORT || 3000;
+const http = require('http');
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log('server listening on port :', port);
+});
