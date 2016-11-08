07a4950014bcb998f1032d86997dec486473b7fc
Tim Combs <tim00combs@gmail.com>
2016-11-07 17:09:49 -0800

    all tests work but population test

--- test/tags.test.js
+++ test/tags.test.js
@@ -6,38 +6,7 @@ chai.use(chaiHttp);
 //to test validation
 const Tag = require('../lib/models/tag');
 
-
-//need connection to db
-const connection = require('../lib/set-mongoose');
-
-const app = require('../lib/app');
-
 describe('the tag model', () => {
-  before((done) => {
-    const CONNECTED = 1;
-    if (connection.readyState === CONNECTED) dropCollection();
-    else connection.on('open', dropCollection);
-
-    function dropCollection() {
-      const name = 'tags';
-      connection.db
-        .listCollections({name})
-        .next((err, callinfo) => {
-          if (err) done(err);
-          if (!callinfo) return done();
-          connection.db.dropCollection(name, done);
-        });
-    }
-  });
-
-  const request = chai.request(app);
-
-  const tagTested =
-    {
-      name: 'tag for testing',
-      description: 'test and learn',
-      heat: 'warm'
-    };
 
   it('validates with name', (done) => {
     const tag = new Tag({
@@ -61,64 +30,4 @@ describe('the tag model', () => {
     });
   });
   
-  it('navigates to POST and stashes a new tag', (done) => {
-    request
-      .post('/tags/tagTested')
-      .send(tagTested)
-      .then((res) => {
-        const tag = res.body;
-        expect(tag.data._id).to.be.ok;
-        tagTested.__v = 0;
-        tagTested._id = tag.data._id;
-        done();
-      })
-      .catch(done);
-  });
-
-  it('navigates to the root and GETs all tags', (done) => {
-    request
-      .get('/')
-      .then((res) => {
-        expect(res.body).to.deep.equal({});
-        done();
-      })
-      .catch(done);
-  });
-
-  it('stashes a tag with no heat', (done) => {
-    request
-      .post('/tags/:id')
-      .send({name: 'empty tag test', description: 'just for testing', heat: ''})
-      .then((res) => {
-        expect(res.body.data._id).to.be.ok;
-        done();
-      })
-      .catch(done);
-  });
-
-  it('updates a tag in the database', (done) => {
-    request
-      .put(`/tags/${tagTested._id}`)
-      .send({name: 'modified tag for testing', description: 'modified tag text', heat: 'warm'})
-      .then((res) => {
-        expect(res.body.data.description).to.deep.equal('modified tag text');
-        done();
-      })
-      .catch(done);
-  });
-
-  it('deletes a tag from the database', (done) => {
-    request
-      .delete(`/tags/${tagTested._id}`)
-      .then(() => {
-        request
-          .get(`/tags/${tagTested._id}`)
-          .then((res) => {
-            expect(res.body.data).to.deep.equal(undefined);
-          });
-        done();
-      })
-      .catch(done);
-  });
-
 });
--- test/web-articles.test.js
+++ test/web-articles.test.js
@@ -6,41 +6,8 @@ chai.use(chaiHttp);
 //to test validation
 const WebArticle = require('../lib/models/web-article');
 
-//need to connect to db
-const connection = require('../lib/set-mongoose');
-
-const app = require('../lib/app');
-
 describe('the web article model', () => {
-  before((done) => {
-    const CONNECTED = 1;
-    if (connection.readyState === CONNECTED) dropCollection();
-    else connection.on('open', dropCollection);
-
-    function dropCollection() {
-      const name = 'web-articles';
-      connection.db
-        .listCollections({name})
-        .next((err, callinfo) => {
-          if (!callinfo) return done();
-          connection.db.dropCollection(name, done);
-        });
-    }
-  });
-
-  const request = chai.request(app);
-
-  const webTested =
-    {
-      title: 'Git it Quick',
-      description: 'Methods for using git to uploading and downloading to github.',
-      url: 'https://www.medium.com/git-it-quick',
-      authorFirst: 'Shelly',
-      authorLast: 'Hackz',
-      publishDate: '2016-10-15',
-      tags: ['git', 'terminal', 'testing']
-    };
-
+  
   it('validates with title, description, url', (done) => {
     const webArticle = new WebArticle({
       title: 'validation web article test',
@@ -63,81 +30,6 @@ describe('the web article model', () => {
       expect(err, 'title should have been required').to.be.ok;
       done();
     });
-  });
-  
-  it('navigates to POST and stashes a new web article', (done) => {
-    request
-      .post('/web-articles')
-      .send(webTested)
-      .then((res) => {
-        const webArticle = res.body;
-        expect(webArticle.data._id).to.be.ok;
-        webTested.__v = 0;
-        webTested._id = webArticle.data._id;
-        done();
-      })
-      .catch(done);
-  });
-
-  it('navigates to the root and GETs all web articles', (done) => {
-    request
-      .get('/web-articles')
-      .then((res) => {
-        expect(res.body).is.ok;
-        done();
-      })
-      .catch(done);
-  });
-
-  it('navigates to /:id and GETs a web article by id', (done) => {
-    request
-      .get(`/web-articles/${webTested._id}`)
-      .then((res) => {
-        const webArticle = res.body;
-        expect(webArticle.data.url).to.deep.equal('https://www.medium.com/git-it-quick');
-        done();
-      })
-      .catch(done);
-  });
-
-  it('stashes a web article with no tags', (done) => {
-    request
-      .post('/web-articles')
-      .send({
-        title: 'empty web article',
-        description: 'an article for testing',
-        url: 'https://www.somewebsite.com',
-      })
-      .then((res) => {
-        expect(res.body.data._id).to.be.ok;
-        done();
-      })
-      .catch(done);
-  });
-
-  it('updates a web article in the database', (done) => {
-    request
-      .put(`/web-articles/${webTested._id}`)
-      .send({title: 'modified web article for testing', description: 'modified description'})
-      .then((res) => {
-        expect(res.body.data.description).to.deep.equal('modified description');
-        done();
-      })
-      .catch(done);
-  });
-
-  it('deletes a web article from the database', (done) => {
-    request
-      .delete(`/web-articles/${webTested._id}`)
-      .then(() => {
-        request
-          .get(`/web-articles/${webTested._id}`)
-          .then((res) => {
-            expect(res.body.data).to.deep.equal(undefined);
-          });
-        done();
-      })
-      .catch(done);
   });
 
 });
