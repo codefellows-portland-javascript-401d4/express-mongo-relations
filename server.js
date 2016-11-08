d5eda679cf4a9ff146dbfeac4e70a7fec2fdc09e
DrewStock <andrewdstock@gmail.com>
2016-11-08 15:39:07 -0800

    set process environment variables, reference '.env.example'

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const app = require('./lib/app');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
+
 require('./lib/setup_mongoose');
 
 const server = http.createServer(app);
 
 server.listen(port, err => {
     if(err) console.log('server error!', err);
-    else console.log('app running on port', server.address().port);
+    else console.log('app running on port', port);
 });
